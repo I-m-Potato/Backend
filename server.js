f6106c81fb6e7b08377790a186ca4c3fecf44520
Yoonjae-Moon <yoonjaemoon20@gmail.com>
2024-05-29 21:49:10 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'auth.html'));
+});
+
+app.get('/emotions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'emotions.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
